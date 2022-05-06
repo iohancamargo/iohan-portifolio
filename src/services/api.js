2b228037b23ed2cd05a2df2dc8630a84d8ef3ab3
06058395909 <iohan.camargo@sevensys.tech>
2022-05-05 21:33:29 -0400

    Add cors in header request recaptcha

--- src/services/api.js
+++ src/services/api.js
@@ -2,4 +2,8 @@ import axios from 'axios';
 
 export const apiReCaptcha = axios.create({
   baseURL: 'https://www.google.com/recaptcha/api',
+  headers: {
+    'Access-Control-Allow-Origin' : '*',
+    'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+  }
 });
